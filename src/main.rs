155b3b69ff11843e6ac181e1d58cf989d6f02b97
Alexander Garcia <agarcia@clarityinnovates.com>
2022-01-08 14:46:57 -0500

    searching for patterns w/ regex

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
+use regex::Regex;
+
 fn main() {
-  let search_term = "picture";
+  // .unwrap(), unwraps a Result (Some, None)
+  let regxr = Regex::new("picture").unwrap();
   let quote = "\
       Every face, every shop, bedroom window, public-house, and 
       dark square is a picture feverishly turned--in search of what?
       It is the same with books.
       What do we seek through millions of pages?";
   
-  for (i, line) in quote.lines().enumerate() {
-    if line.contains(search_term) {
-      let line_num = i + 1;
-      println!("#{} | {}", line_num, line)
+  for line in quote.lines() {
+    let contains_substr = regxr.find(line);
+    match contains_substr {
+      Some(_) => println!("{}", line),
+      None => ()
     }
   }
 }
