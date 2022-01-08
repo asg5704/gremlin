5e0cea1aa34f0a3b1b35854eb4f4a198024babac
Alexander Garcia <agarcia@clarityinnovates.com>
2022-01-08 15:04:08 -0500

    builds cli with args

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,32 @@
+use clap::{App, Arg};
 use regex::Regex;
 
 fn main() {
-  // .unwrap(), unwraps a Result (Some, None)
-  let regxr = Regex::new("picture").unwrap();
-  let quote = "\
+    let args = App::new("gremlin")
+        .version("0.1")
+        .about("Grep-like searcher for patterns")
+        .arg(
+            Arg::with_name("pattern")
+                .help("Pattern to search for")
+                .takes_value(true)
+                .required(true),
+        )
+        .get_matches();
+
+    // .unwrap(), unwraps a Result (Some, None)
+    let pattern = args.value_of("pattern").unwrap();
+    let regxr = Regex::new(pattern).unwrap();
+
+    let quote = "\
       Every face, every shop, bedroom window, public-house, and 
       dark square is a picture feverishly turned--in search of what?
       It is the same with books.
       What do we seek through millions of pages?";
-  
-  for line in quote.lines() {
-    let contains_substr = regxr.find(line);
-    match contains_substr {
-      Some(_) => println!("{}", line),
-      None => ()
+
+    for line in quote.lines() {
+        match regxr.find(line) {
+            Some(_) => println!("{}", line),
+            None => (),
+        }
     }
-  }
 }
