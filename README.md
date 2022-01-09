70ec92ad7362850765483d472da43b01f94452e4
Alexander Garcia <alexander.schmidt.garcia@gmail.com>
2022-01-09 18:08:37 -0500

    adds readme & test file

---
+++ README.md
@@ -0,0 +1,49 @@
+# 👹 Gremlin
+
+A lightweight, grep-like searcher written in Rust that uses a gremlin to finds patterns.
+
+## Motivation
+
+- As a means of learning the Rust programming language
+
+## Example
+
+// TODO
+
+## Installation
+
+To install `gremlin`, you can download the binaries [here](https://github.com/asg5704/gremlin/releases). Remember to puth the path to the binary in your `PATH`.
+
+## Usage
+
+```
+$ gremlin -h
+gremlin 0.1.0
+Alexander Garcia <https://alexandergarcia.me>
+A grep-like pattern searcher
+
+USAGE:
+    gremlin <pattern> [input]
+
+ARGS:
+    <pattern>    Pattern to search for
+    <input>      File to search [optional]
+
+OPTIONS:
+    -h, --help       Print help information
+    -V, --version    Print version information
+```
+
+### _Search within a file_
+
+```bash
+gremlin <pattern> <input-file>
+gremlin Banana test.txt
+```
+
+### _Search using pipe_
+
+```bash
+... | gremlin <pattern>
+cat test.txt | gremlin Banana
+```
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 use clap::{App, Arg};
 use regex::Regex;
 use std::fs::File;
+use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
 
+fn process_lines<T: BufRead + Sized>(reader: T, re: Regex) {
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        match re.find(&line) {
+            Some(_) => println!("{}", line),
+            None => (),
+        }
+    }
+}
+
 fn main() {
     let args = App::new("gremlin")
-        .version("0.1")
-        .about("Grep-like searcher for patterns")
+        .version("0.1.0")
+        .about("👹 A grep-like pattern searcher")
+        .author("Alexander Garcia <https://alexandergarcia.me/contact>")
         .arg(
             Arg::new("pattern")
                 .help("Pattern to search for")
@@ -16,24 +28,37 @@ fn main() {
         )
         .arg(
             Arg::new("input")
-                .help("File to search")
+                .help("File to search [optional]")
                 .takes_value(true)
-                .required(true),
+                .required(false),
         )
         .get_matches();
 
-    // .unwrap(), unwraps a Result (Some, None)
-    let pattern = args.value_of("pattern").unwrap();
-    let file_name = args.value_of("input").unwrap();
-    let regxr = Regex::new(pattern).unwrap();
-    let search_file = File::open(file_name).unwrap();
-    let reader = BufReader::new(search_file);
+    // Creates input if file passed or default stdin
+    let input = args.value_of("input").unwrap_or("-");
 
-    for line_ in reader.lines() {
-        let line = line_.unwrap();
-        match regxr.find(&line) {
-            Some(_) => println!("{}", line),
-            None => (),
-        }
+    // Creates the pattern for regexr
+    let pattern = args.value_of("pattern").unwrap();
+
+    // Creates new Regex from pattern
+    let regxr = Regex::new(pattern).unwrap();
+
+    // Determines if file_name passed or stdin
+    if input != "-" {
+        // Creates search_file if file passed
+        let search_file = File::open(input).unwrap();
+
+        // Creates the BufferReader for search term
+        let reader = BufReader::new(search_file);
+
+        process_lines(reader, regxr);
+    } else {
+        // Takes input from stdin
+        let stdin = io::stdin();
+
+        // Creates a reader from stdin
+        let reader = stdin.lock();
+
+        process_lines(reader, regxr);
     }
 }
